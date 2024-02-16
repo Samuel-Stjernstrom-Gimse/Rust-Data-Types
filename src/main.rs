4db1e65b42db41a2f573bd637c6a2eb43fd1a583
Zam <samuel.kodehode@gmail.com>
2024-02-16 15:38:01 +0100

    1.0.0

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x: i32 = 2;
+    let floating_point: f64 = 2.44;
+
+    let true_or_false: bool = false;
+
+    let letter: char = 'a';
+
+
+    let mut tupel: (i32, bool, char) = (1, true, 's');
+
+    tupel.0 = 56;
+
+    println!("{}", tupel.0);
+
+    const HELLO: &str = "hey";
+
+   let mut array: [i32; 5]  = [1,2,3,4,5];
+
+    array[4] = 12;
+
+    println!("{}", array[4]);
+
+    let x: u8 = 4;
+
+
+
+
+}
